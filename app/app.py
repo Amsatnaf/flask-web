d4e088b194bc11ea96fe340478a5a70986357c11
Amsatnaf <diego.alme@hotmail.com>
2026-01-18 00:05:28 +0000

    Fim dos testes de ERRO

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
    ##return 'Olá, mundo! 👋', 200
-   return 'Olá, Rancher Fleet! Agora vai.. Testes de automatização! 🚀', 200 - ERRO
+   return 'Olá, CI - CD com Rancher Fleet e GitHub! 🚀', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
